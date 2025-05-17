e3ced93b25826485039d2d18cc61665d7df88870
lizkook1997 <mesonali.sharma99@gmail.com>
2025-05-18 02:18:14 +0530

    updated

--- merge-disct.py
+++ merge-disct.py
@@ -6,3 +6,8 @@ merged_dict = {**{k: v for k, v in disct1.items() if k in 'aeiou'},
 
 # merged_dict = {**disct2, **disct1, }
 print(merged_dict)
+
+# we are filtering both dictionaries to include only keys that are vowels.
+# Then we merged those filtered results using dictionary unpacking (**).
+# Only 'a' from disct1 and 'e' from disct2 are vowels.
+# Final output is: {'a': 1, 'e': 5}
